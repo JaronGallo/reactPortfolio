b603a7fc3c33a719c3bc0b311a396d64886a26f5
Jaron Gallo <you@example.com>
2019-01-06 19:25:25 -0700

    server

--- server.js
+++ server.js
@@ -67,6 +67,6 @@ app.get('*', (req, res) => {
 
 
 // Dont forget to change back to 80 also proxy to 80
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 80);
 
 console.log("works");
