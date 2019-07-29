8ebc2cf7f3eec865ea9066f5a1acde87ef3b8590
Jaron Gallo <you@example.com>
2019-07-28 20:09:36 -0600

    server testing

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+
 const path = require('path');
 
 const app = express();
@@ -12,52 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-
-app.post('/api/form', (req, res) => {
-  
-    const htmlEmail = `
-    <h3> Contact Detail</h3>
-    <ul>
-    <li>Name: ${req.body.name}</li>
-    <li>Email: ${req.body.email}</li>
-    <p>Message: ${req.body.message}</p>
-    </ul>
-    `;
-
-    let transporter = nodemailer.createTransport({
-      host: 'smtp.gmail.com',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: { 
-          user: '',
-        pass:''
-        }
-    });
-
-    let mailOptions = {
-      from: 'jarongallo@gmail.com',
-      to: 'contactjarongallo@gmail.com',
-      subject: 'New Message',
-      text: req.body.message,
-      html: htmlEmail
-    }
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log('Message sent: %s', info.message);
-      // Preview only available when sending through an Ethereal account
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-      res.render("contact", {msg:"email sent!"});
-
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
-
-
-});
 
 
 app.get('*', (req, res) => {
